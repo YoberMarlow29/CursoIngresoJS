87172844c49c34c3d6f4e8bb55d93cff8377e813
YoberMarlow29 <98592665+YoberMarlow29@users.noreply.github.com>
2022-01-31 12:28:28 -0300

    Ejercicios 1-5

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,10 @@
 /*
+Vasquez Panduro Yober Marlow DIV E
+E/S ej1
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Esto funciona como de maravilla");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,29 @@
 /*
+Vasquez Panduro Yober Marlow
+e/s ej3
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	//declaramos variable
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	
+	//guardamos el nombre
+	//nombreIngresado = document.getElementById("txtIdNombre").value;
+	
+	nombreIngresado = txtIdNombre.value;
+	//mostrarlo por alert
+	alert("su nombre es " + nombreIngresado);
 
 }
+//txtIdNombre
+/*
+entrada:
+-promt:
+-id-
+procesos:
 
+salida:
+-alert
+*/
 
